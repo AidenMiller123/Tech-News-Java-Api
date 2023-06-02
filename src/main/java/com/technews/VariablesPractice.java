4a2c0ad6012cb637f326857e2753519243cab662
AidenMiller123 <123018143+AidenMiller123@users.noreply.github.com>
2023-06-01 23:04:30 -0400

    Built structure of the database

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,14 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int num1 = 5;
+        double num2 = 7.5;
+        double sum = num1 + num2;
+        System.out.println(sum);
+
+        String name1 = "Aiden";
+        String result = name1 + " " + num1;
+        System.out.println(result);
+    }
+}
